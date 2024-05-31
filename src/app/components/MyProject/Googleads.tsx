e7d747fd2505e87ac92cafc8f9063d199e84e121
hugojohansson <carlhugojohansson@outlook.com>
2024-05-31 20:13:53 +0200

    Add - New Projects

--- src/app/components/MyProject/Googleads.tsx
+++ src/app/components/MyProject/Googleads.tsx
@@ -11,6 +11,8 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
         { src: "Keyword_algo.png", alt: "Profil img 1" },
         { src: "Keyword_sheet_1.png", alt: "Profil img 2" },
         { src: "Keyword_sheet_2.png", alt: "Profil img 3" },
+        { src: "Flixtrackr_SEO_ranks.PNG", alt: "Profil img 4" },
+        { src: "SEO_score.png", alt: "Profil img 5" },
       ];
     
       const [activeIndex, setActiveIndex] = useState(0);
@@ -48,7 +50,7 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
           <div className="flex flex-col justify-center">
             
           
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center scale-75">
   <motion.img
     key={images[activeIndex].src}
     src={images[activeIndex].src}
@@ -57,7 +59,7 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
     animate={{ opacity: 1, scale: 1 }}
     exit={{ opacity: 0, scale: 0.8 }}
     transition={{ duration: 0.6 }}
-    className="w-full h-[500px]  rounded-3xl shadow-lg"
+    className="w-full h-fit  rounded-3xl shadow-lg"
   />
 </div>
 
@@ -74,7 +76,7 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
               className="bg-white rounded-3xl shadow-lg p-4 w-full mt-28"
             >
               <h3 className="text-2xl font-bold text-black mb-4">
-               {isSwedish ? 'Google ads' : 'Google ads'}
+               {isSwedish ? 'Google ads och SEO' : 'Google ads and SEO'}
               </h3>
 
             <p className='text-black italic '>
@@ -85,6 +87,14 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
                 {isSwedish ? 'Denna algorithm räknade ut vilka sökord på google och Meta som var undervärderade på marknaden inom specificka nischer, detta resulterade till mindre kostnader men samtidigt mer trafik till hemsidan och mer konverteringar ' : 'This algorithm calculated fold keywords on google and Meta that were undervalued on the market within specific niches, this resulted in lower costs but at the same time more traffic to the website and more conversions'}
             </p>
             <br />
+            <br />
+            <p className='text-black italic '>
+                {isSwedish ? 'Eftersom jag har ett intresse av att bygga hemsidor och byggt hemsidor i många år så har SEO varigt en viktig del under tiden, Jag har sedan många år tillbaka förstått värdet i att en hemsida ska vara upp till google vitals standard samt hur man sökords placerar effektivt på hemsidor för att google ska ranka hemsidan högt  ' : 'Since I have an interest in building websites and have built websites for many years, SEO has always been an important part in the meantime, I have for many years understood the value of a website being up to google vitals standard and how to place keywords effectively on websites for Google to rank the website highly'}
+            </p>
+            <br />
+            <p className='text-black italic '>
+                {isSwedish ? 'Under min tid på Supernormal fick jag också ägande att hålla siten upp till marknads standard, detta uppnådes genom att använda verktyg som "Ubersuggest", "LightHouse", "arhefs" och andra relevanta verktyg för att säkerställa optimal SEO och Webbsite Performace ' : 'During my time at Supernormal I also took ownership of keeping the site up to market standards, this was achieved by using tools such as "Ubersuggest", "LightHouse", "arhefs" and other relevant tools to ensure optimal SEO and Website Performace'}
+            </p>
             <div className="mt-6 flex flex-wrap justify-start space-x-4">
   <img
     src="Google.png"
@@ -96,6 +106,17 @@ const Googleads: React.FC<{ isSwedish: boolean; setIsSwedish: React.Dispatch<Rea
     alt="google ad Icon"
     className="w-12 h-12"
   />
+  <img
+    src="Ahrefs-Logo.png"
+    alt="Ahrefs Icon"
+    className="w-12 h-12"
+  />
+  <img
+    src="googleana.png"
+    alt="next Icon"
+    className="w-13 h-12"
+  />
+
   
   
 </div>
